857a25eddfc664bf20a3edca334efc8a4b564902
Raynaldi <raynss455x@gmail.com>
2024-11-15 10:31:53 +0700

    Be: Add Cors

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,18 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  app.useGlobalPipes(new ValidationPipe());
+
+  // Atur CORS untuk mengizinkan permintaan dari origin tertentu
+  app.enableCors({
+    origin: 'http://localhost:5173', // ganti dengan origin frontend Anda
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+    credentials: true,
+  });
+
+  await app.listen(3000);
 }
 bootstrap();
